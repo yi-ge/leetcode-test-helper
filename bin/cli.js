ec7cc431665bde1260a7c3f9194834468c31c11e
yi-ge <a@wyr.me>
2022-09-21 16:18:34 +0800

    chore: 重命名仓库

--- bin/cli.js
+++ bin/cli.js
@@ -1,15 +1,33 @@
 #!/usr/bin/env node
 import os from 'os'
+import path from 'path'
+import { spawn } from 'child_process'
 
 // 环境检测
-if (os.type() !== 'Darwin' && os.type() !== 'Linux' && os.type() !== 'Windows_NT') {
+if (
+  os.type() !== 'Darwin' &&
+  os.type() !== 'Linux' &&
+  os.type() !== 'Windows_NT'
+) {
   console.log('当前操作系统：', os.type())
   console.log('暂仅支持MacOS、Linux和Windows使用此脚本')
   exit(0)
 }
-
-if (os.type() === 'Windows_NT') {
-
-} else {
-
-}
+console.log(process.argv)
+console.log(process.argv.slice(3))
+spawn(
+  'node',
+  [
+    '--no-warnings',
+    '--experimental-vm-modules',
+    '--experimental-specifier-resolution=node',
+    '--loader',
+    'ts-node/esm',
+    path.join(process.cwd(), 'node_modules/leetcode-test-helper/src/main.ts'),
+    process.argv.slice(3),
+  ],
+  {
+    stdio: 'inherit',
+    shell: os.type() === 'Windows_NT',
+  }
+)
