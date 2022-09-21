6a313bad5b3f2cb5b9139811eff0083033144598
yi-ge <a@wyr.me>
2022-09-21 18:11:37 +0800

    test: leetcode-test-helper

--- bin/cli.js
+++ bin/cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import os from 'os'
 import path from 'path'
-import { spawn } from 'child_process'
+import { spawnSync } from 'child_process'
 
 // 环境检测
 if (
@@ -13,21 +13,29 @@ if (
   console.log('暂仅支持MacOS、Linux和Windows使用此脚本')
   exit(0)
 }
-console.log(process.argv)
-console.log(process.argv.slice(3))
-spawn(
+
+process.env.TS_NODE_PROJECT = path.normalize(
+  'node_modules/leetcode-test-helper/tsconfig.json'
+)
+
+spawnSync(
   'node',
   [
     '--no-warnings',
+    '--input-type=module',
     '--experimental-vm-modules',
     '--experimental-specifier-resolution=node',
+    '--experimental-network-imports',
     '--loader',
     'ts-node/esm',
-    path.join(process.cwd(), 'node_modules/leetcode-test-helper/src/main.ts'),
-    process.argv.slice(3),
+    path.normalize('node_modules/leetcode-test-helper/src/main.ts'),
+    process.argv.slice(2),
   ],
   {
     stdio: 'inherit',
-    shell: os.type() === 'Windows_NT',
+    cwd: process.cwd(),
+    shell: true,
+    windowsHide: true,
+    env: process.env,
   }
 )
